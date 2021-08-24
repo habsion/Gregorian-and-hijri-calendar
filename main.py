74b52bdb7208a93e8d1a4b9c3a2d334584a028a4
Habeeb Mohammad <69910360+habsion@users.noreply.github.com>
2021-08-24 23:37:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+import calendar
+from hijri_converter import Gregorian
+y=int(input('Enter year: '))
+m=int(input('Enter month as a number: '))
+mon=calendar.month(y,m).split('\n')
+print('\n'.join(mon[:2]),'\n','-'*20,sep='')
+t=len(mon[2].split())
+print(mon[2],'\n',' '*3*(7-t),' '.join(str(Gregorian(y, m, i).to_hijri()).split('-')[2] for i in range(1,t+1)),sep='')
+for i in mon[3:]:
+    print('-'*20+'\n',i,'\n',' '.join(str(Gregorian(y, m, int(j)).to_hijri()).split('-')[2] for j in i.split()),sep='')
