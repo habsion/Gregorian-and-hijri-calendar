7f47701849b150890215134e327187ba4b0b40ac
Habeeb Mohammad <69910360+habsion@users.noreply.github.com>
2021-08-25 00:31:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ month_dict = {1: 'محرم', 2: 'صفر', 3: 'ربيع الأول', 4: 'ربيع
 , 8: 'شعبان', 9: 'رمضان', 10: 'شوال', 11: 'ذو القعدة', 12: 'ذو الحجة'}
 
 y=int(input('Enter year: '))
-m=int(input('Enter month as a number: '))
-mon=calendar.month(y,m).split('\n')
-print(mon[0])
-print(hijri_month_name(int(mon[-2].rstrip()[-2::])))
-t=len(mon[2].split())
-print(mon[2],'\n',' '*3*(7-t),' '.join(str(Gregorian(y, m, i).to_hijri()).split('-')[2] for i in range(1,t+1)),sep='')
-for i in mon[3:]:
-    print('-'*20+'\n',i,'\n',' '.join(str(Gregorian(y, m, int(j)).to_hijri()).split('-')[2] for j in i.split()),sep='')
+for m in range(1,13):
+    mon=calendar.month(y,m).split('\n')
+    print(mon[0])
+    print(hijri_month_name(int(mon[-2].rstrip()[-2::])))
+    t=len(mon[2].split())
+    print(mon[2],'\n',' '*3*(7-t),' '.join(str(Gregorian(y, m, i).to_hijri()).split('-')[2] for i in range(1,t+1)),sep='')
+    for i in mon[3:]:
+        print('-'*20+'\n',i,'\n',' '.join(str(Gregorian(y, m, int(j)).to_hijri()).split('-')[2] for j in i.split()),sep='')
