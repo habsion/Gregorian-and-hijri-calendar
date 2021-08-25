748528b0dc7a16054626107514e1edd01abd90d1
Habeeb Mohammad <69910360+habsion@users.noreply.github.com>
2021-08-26 00:19:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import calendar
 from hijri_converter import Gregorian
+import openpyxl
 
-
-def hijri_month_name(a):
+def hijri_month_range(a):
     return (month_dict[int(str(Gregorian(y, m, 1).to_hijri()).split('-')[1])]+' - '+month_dict[int(str(Gregorian(y, m, a).to_hijri()).split('-')[1])]).center(20)
 
 
@@ -10,13 +10,22 @@ month_dict = {1: 'محرم', 2: 'صفر', 3: 'ربيع الأول', 4: 'ربيع
 , 8: 'شعبان', 9: 'رمضان', 10: 'شوال', 11: 'ذو القعدة', 12: 'ذو الحجة'}
 
 y=int(input('Enter year: '))
+wb=openpyxl.load_workbook('D:\\Python\\Calendar.xlsx')
 for m in range(1,13):
     mon=calendar.month(y,m).split('\n')
-    print(mon[0])
-    print(hijri_month_name(int(mon[-2].rstrip()[-2::])))
-    print(mon[1])
-    t=len(mon[2].split())
-    print(mon[2],'\n',' '*3*(7-t),' '.join(str(Gregorian(y, m, i).to_hijri()).split('-')[2] for i in range(1,t+1)),sep='')
-    for i in mon[3:-1]:
-        print('-'*20+'\n',i,'\n',' '.join(str(Gregorian(y, m, int(j)).to_hijri()).split('-')[2] for j in i.split()),sep='')
-    print('\n')
+    sheet=wb[str(m)]
+    sheet.cell(1,4,value=mon[0].strip())
+    sheet.cell(2,4,hijri_month_range(int(mon[-2].rstrip()[-2::])))
+    mon[1]=mon[1].split()
+    for i in range(1,8):
+        sheet.cell(3,i,value=mon[1][i-1])
+    mon[2]=mon[2].split()
+    mon[2]=['']*(7-len(mon[2]))+mon[2]
+    for i in range(1,8):
+        if mon[2][i-1] != '':
+            sheet.cell(4,i,mon[2][i-1]+' | '+str(Gregorian(y, m, int(mon[2][i-1])).to_hijri()).split('-')[2])
+    for i in range(5,5+len(mon[3:-1])):
+        mon[i-2]=mon[i-2].split()
+        for j in range(1,len(mon[i-2])+1):
+            sheet.cell(i,j,mon[i-2][j-1]+' | '+str(Gregorian(y, m, int(mon[i-2][j-1])).to_hijri()).split('-')[2])
+wb.save('D:\\Python\\Calendar.xlsx')
